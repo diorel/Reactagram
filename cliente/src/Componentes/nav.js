431c876c08d9cf8dbd7206be8901f1d66b0df03e
Diorelyon Amador <diorel_x@hotmail.com>
2020-08-15 11:21:19 -0500

    Proyecto Reactagram
    este proyecto es un clon de Instagram

---
+++ cliente/src/Componentes/nav.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCameraRetro } from '@fortawesome/free-solid-svg-icons';
+import { faCompass , faUser } from '@fortawesome/free-regular-svg-icons';
+
+export default function Nav({ usuario }) {
+  return (
+    <nav className="Nav">
+      <ul className="Nav__links">
+        <li>
+          <Link className="Nav__link" to="/">
+            Clontagram
+          </Link>
+        </li>
+        {usuario && <LoginRoutes usuario={usuario} />}
+      </ul>
+    </nav>
+  );
+}
+
+function LoginRoutes({ usuario }) {
+  return (
+    <>
+      <li className="Nav__link-push">
+        <Link className="Nav__link" to="/upload">
+          <FontAwesomeIcon icon={faCameraRetro} />
+        </Link>
+      </li>
+      <li className="Nav__link-margin-left">
+        <Link className="Nav__link" to="/explore">
+          <FontAwesomeIcon icon={faCompass} />
+        </Link>
+      </li>
+      <li className="Nav__link-margin-left">
+        <Link className="Nav__link" to={`/perfil/${usuario.username}`}>
+          <FontAwesomeIcon icon={faUser} />
+        </Link>
+      </li>
+    </>
+  );
+}
---
+++ cliente/src/Vistas/Signup.js
@@ -0,0 +1,105 @@
+import React, {useState} from 'react';
+import Main from '../Componentes/Main';
+import imagenSignup from '../imagenes/signup.png';
+import { Link }  from 'react-router-dom';
+
+
+export default function Signup( {signup , mostrarError}){
+  
+const [usuario, setUsuario] = useState({
+    email:'',
+    username:'',
+    password:'',
+    bio:'',
+    nombre:''
+});
+
+function handleInputChange(e) {
+  setUsuario({
+    ...usuario,
+    [e.target.name]: e.target.value
+  });
+}
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  try{
+  await signup(usuario);
+  }
+  catch(error){
+    mostrarError(error.response.data);
+    console.log(error);
+  }
+}  
+
+
+  return(
+    <Main center={true}>
+       <div className="Signup">
+          <img src={imagenSignup} alt="" className="Signup__img" />
+          <div className="FormContainer">
+            <h1 className="Form__titulo">Clontagram</h1>
+            <p className="FormContainer__info">
+               Regístrate para que veas este clon de Instagram
+            </p>
+            <form onSubmit={handleSubmit}>
+               <input
+                 type="email"
+                 name="email"
+                 placeholder="Email"
+                 className="Form__field"
+                 required
+                 onChange={handleInputChange}
+                 value={usuario.email}
+               />
+              <input
+                 type="text"
+                 name="nombre"
+                 placeholder="Nombre y Apellido"
+                 className="Form__field"
+                 required
+                 minLength="3"
+                 maxLength="100"
+                 onChange={handleInputChange}
+                 value={usuario.nombre}
+               />
+              <input
+                 type="text"
+                 name="username"
+                 placeholder="Username"
+                 className="Form__field"
+                 required
+                 minLength="3"
+                 maxLength="30"
+                 onChange={handleInputChange}
+                 value={usuario.username}
+               />
+              <input
+                 type="text"
+                 name="bio"
+                 placeholder="Cuéntanos de ti..."
+                 className="Form__field"
+                 required
+                 maxLength="150"
+                 onChange={handleInputChange}
+                 value={usuario.bio}
+               />
+              <input
+                 type="password"
+                 name="password"
+                 placeholder="Contraseña"
+                 className="Form__field"
+                 required
+                 onChange={handleInputChange}
+                 value={usuario.password}
+               />
+               <button className="Form__submit" type="submit">Sign up</button>
+               <p className="FormContainer__info">
+                  Ya tienes cuenta? <Link to="/login">Login</Link>
+               </p>
+            </form>
+          </div>
+       </div>     
+    </Main>  
+  );
+}
